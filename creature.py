0ab99fd3b92692dd037c8e151bfdaa6b5e499d14
Aidan Simon <aidan@Omri.mynetworksettings.com>
2025-05-15 17:32:54 -0400

    Added code to streamline creture and environment implementation.

--- creature.py
+++ creature.py
@@ -1,24 +1,63 @@
 # TODO: create a prototype for the simulated creature
 import random
-
-def getThresh(loci):
-    acc = 0
-    for i in loci:
-        if i == True:
-            acc = acc + 1
-        else:
-            continue
-    return ((100 - (acc * 20)))
-
+class Genome:
+    threshold = 0
+    def __init__(self):
+        self.genes = []
+         
     
-def makeGenome(loci):
-     return {
-         "genetics" : loci,
-         "thresh" : getThresh(loci)
-         }
+    def calcThresh(self):
+         acc = 0
+         for i in self.genes:
+              if i == True:
+                   acc += 1
+              else:
+                   continue
+              self.threshold = (100 - (20 * acc))
 
+    def makeRandomGenome(self):
+        i = 0
+        while i < 5:
+             self.genes.append(bool(random.randint(0,1)))
+             i += 1
+        self.calcThresh()
+
+    def mutate(self):
+        if len(genes) != 0:
+            chance = random.randint(0, 100)
+            if chance > 25:
+                i = random.randint(0,4)
+                self.genes[i] = not(self.genes[i])
+                self.calcThresh()
+                
+    def getThresh(self):
+          return self.threshold
+
+    def print(self):
+        print("Gene summary\n", self.genes, "\n", self.threshold)
+        
+    
+
+ 
 class Creature:
-    def __init__(self, idnum, genome):
+    def __init__(self, idnum):
         self.idNum = idnum
-        self.genome = genome
+        self.genome = Genome()
+
+    def setRandomGenome(self):
+        self.genome.makeRandomGenome()
+
+    def mutate(self):
+        self.genome.mutate()
+        
+    def getId(self):
+        return self.idNum
+
+    def getThresh(self):
+        return self.genome.getThresh()
     
+    def print(self):
+        print("Creature id: ", self.idNum, "\n",
+              "Creature threshold: ", self.getThresh())
+        print(self.genome.genes)
+#        self.genome.print()
--- environment.py
+++ environment.py
@@ -1,1 +1,34 @@
 # TODO: add code to simulate environment for the creatures
+import creature as c
+import random as rand
+class Environment:
+    temp = 0
+    creatures = []
+    def __init__(self):
+        self.temp = rand.randint(1, 100)
+
+
+    def populate(self, n): # populate environment with n # of creatures
+        acc = 0
+        while acc < n:
+            self.creatures.append(c.Creature(acc))
+            self.creatures[acc].setRandomGenome()
+            acc += 1
+
+
+    def cullPopulation(self): # checks to see if creatures in enviornment survive
+        chance = rand.randint(1, 100)
+        acc = 0
+        for i in self.creatures:
+            if self.creatures[acc].getThresh() < self.temp:
+                if chance < 50:
+                   self.creatures.remove(i)
+            acc += 1
+
+    def print(self):
+        print("Environment summary")
+        print("Temperature: ", self.temp)
+
+        for i in self.creatures:
+            i.print()
+        
