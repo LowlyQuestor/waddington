693660fb91803ca045aff16b3199ee6353ee3b20
Aidan Simon <aidan@Omri.mynetworksettings.com>
2025-05-15 13:33:38 -0400

    Added code for creature

--- creature.py
+++ creature.py
@@ -1,1 +1,24 @@
 # TODO: create a prototype for the simulated creature
+import random
+
+def getThresh(loci):
+    acc = 0
+    for i in loci:
+        if i == True:
+            acc = acc + 1
+        else:
+            continue
+    return ((100 - (acc * 20)))
+
+    
+def makeGenome(loci):
+     return {
+         "genetics" : loci,
+         "thresh" : getThresh(loci)
+         }
+
+class Creature:
+    def __init__(self, idnum, genome):
+        self.idNum = idnum
+        self.genome = genome
+    
