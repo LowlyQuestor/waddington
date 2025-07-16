c55aaee816c7c8a09ad59d3bee34d957fb21ccb0
Aidan Simon <aidan@Omri.myfiosgateway.com>
2025-07-15 20:49:01 -0400

    Improved python style

--- environment.py
+++ environment.py
@@ -2,15 +2,16 @@
 import creature as c
 import random as rand
 
+
 class Environment:
     population_cap = 5
     creatures = []
-    
+
     def __init__(self):
         self.temp = rand.randint(1, 100)
         self.totalCreaturesEver = 0
 
-    def populate(self, n): # populate environment with n # of creatures
+    def populate(self, n):  # populate environment with n # of creatures
         acc = 0
         while acc < n:
             self.creatures.append(c.Creature(acc))
@@ -18,15 +19,15 @@ class Environment:
             acc += 1
             self.totalCreaturesEver += 1
 
-    def cullPopulation(self): # checks to see if creatures in enviornment survive
+    def cullPopulation(self):  # checks to see if creatures in enviornment survive
         chance = rand.randint(1, 100)
         acc = 0
         for i in self.creatures:
             myId = self.creatures[acc].getId()
             if self.creatures[acc].getThresh() < self.temp:
                 if chance < 50:
-                   print("creature " + str(myId) + " has perished")
-                   self.creatures.remove(i)
+                    print("creature " + str(myId) + " has perished")
+                    self.creatures.remove(i)
             acc += 1
 
     def reproduction(self):
@@ -42,11 +43,10 @@ class Environment:
             acc += 1
 
     def reproduceCreature(self, parent):
-        newId = len(self.creatures) + 1 # id of new creature is one after last creature
+        newId = len(self.creatures) + 1  # id of new creature is one after last
         temp = c.Creature(newId)
         temp.setOffspringGenomeAce(parent)
         self.creatures.append(temp)
-            
 
     def print(self):
         print("\nEnvironment summary")
@@ -58,7 +58,8 @@ class Environment:
             print("\n")
 
     def setRandTemp(self):
-        self.temp = rand.randint(1,100)
+        self.temp = rand.randint(1, 100)
+
 
 p = Environment()
 p.populate(5)
